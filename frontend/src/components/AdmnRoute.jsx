862facc847e18844b2a6ec4193bf75a89b42e09b
Mujjuni Brian <brianmujjuni@engineer.com>
2024-06-04 18:17:36 +0300

    restrict admin route component

---
+++ frontend/src/components/AdmnRoute.jsx
@@ -0,0 +1,11 @@
+import { Outlet, Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+export default function AdmnRoute() {
+  const { userInfo } = useSelector((state) => state.auth);
+  return userInfo && userInfo.isAdmin ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/login" replace />
+  );
+}
