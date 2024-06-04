63726e91a34d547526dc0c5aafd36229a32625b5
Mujjuni Brian <brianmujjuni@engineer.com>
2024-06-04 19:42:14 +0300

    remove console log

--- frontend/src/screens/Admin/OrderListScreen.jsx
+++ frontend/src/screens/Admin/OrderListScreen.jsx
@@ -7,7 +7,7 @@ import { useGetOrdersQuery } from "../../slices/ordersApiSlice";
 
 export default function OrderListScreen() {
   const { data: orders, isLoading, error } = useGetOrdersQuery();
-  console.log(orders);
+
   return (
     <>
       <h1>Orders</h1>
