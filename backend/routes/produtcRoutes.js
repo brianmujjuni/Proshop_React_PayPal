3b4d025b066965265b09640251cabf45893e7583
Mujjuni Brian <brianmujjuni@engineer.com>
2024-06-05 18:49:07 +0300

    create product route

--- backend/routes/produtcRoutes.js
+++ backend/routes/produtcRoutes.js
@@ -4,12 +4,13 @@ import {
   getProductById,
   getProducts,
   createProduct,
+  updateProduct,
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authmiddleware.js";
 
 const router = express.Router();
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
-router.route("/:id").get(getProductById);
+router.route("/:id").get(getProductById).put(protect, admin, updateProduct);
 
 export default router;
