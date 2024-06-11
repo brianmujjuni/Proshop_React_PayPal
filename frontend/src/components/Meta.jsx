309197685d0596d92122a5c85ce4d8c34af2728c
Mujjuni Brian <brianmujjuni@engineer.com>
2024-06-11 19:33:00 +0300

    create meta component

---
+++ frontend/src/components/Meta.jsx
@@ -0,0 +1,16 @@
+import { Helmet } from "react-helmet-async";
+
+export default function Meta({ title, description, keywords }) {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="keywords" content={keywords} />
+    </Helmet>
+  );
+}
+Meta.default = {
+  title: "Welcome To Proshop",
+  description: "We sell the best products for cheap",
+  keywords: "electronics, buy electronics, cheap electronics",
+};
