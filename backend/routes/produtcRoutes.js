ac3adb3c6f67ce7be684c788869067f5a7f8d209
Mujjuni Brian <brianmujjuni@engineer.com>
2024-06-11 18:59:03 +0300

    create top rated products route

--- backend/routes/produtcRoutes.js
+++ backend/routes/produtcRoutes.js
@@ -7,12 +7,14 @@ import {
   updateProduct,
   deleteProduct,
   createProductReview,
+  getTopProducts,
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authmiddleware.js";
 
 const router = express.Router();
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
+router.get("/top", getTopProducts);
 router
   .route("/:id")
   .get(getProductById)
